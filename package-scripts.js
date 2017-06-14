b51299c8d60a56c182459925e6288f6ae7498742
gallayl <gallay.lajos@gmail.com>
2017-06-14 16:57:28 +0200

    chore(package-scripts): added host setting to webpack-dev-server

--- package-scripts.js
+++ package-scripts.js
@@ -84,7 +84,7 @@ module.exports = {
         }
       },
       server: {
-        default: `webpack-dev-server -d --devtool '#source-map' --inline --env.server`,
+        default: `webpack-dev-server -d --devtool '#source-map' --inline --env.server --host snbppc109`,
         extractCss: `webpack-dev-server -d --devtool '#source-map' --inline --env.server --env.extractCss`,
         hmr: `webpack-dev-server -d --devtool '#source-map' --inline --hot --env.server`
       },
