f9134a1a581e9a0b07bb80f374e64553e906f7c8
gallayl <gallay.lajos@gmail.com>
2017-08-02 17:57:45 +0200

    fix(Content-Logger): fixed repo.Events namespacing

--- src/utils/content-logger.ts
+++ src/utils/content-logger.ts
@@ -8,30 +8,30 @@ export class ContentLogger {
     private readonly toasTimeout: number = 4000;
 
     constructor(private repo: Repository.BaseRepository, toast: MdToastService) {
-        this.repo.OnContentLoaded.subscribe(p => {
+        this.repo.Events.OnContentLoaded.subscribe(p => {
             console.log('OnContentLoaded', p);
         });
 
-        this.repo.OnContentCreated.subscribe(mod => {
+        this.repo.Events.OnContentCreated.subscribe(mod => {
             console.log("OnContentCreated", mod);
             toast.show('Content created', this.toasTimeout, 'green');
         });
-        this.repo.OnContentCreateFailed.subscribe(mod => {
+        this.repo.Events.OnContentCreateFailed.subscribe(mod => {
             console.log("OnContentCreateFailed", mod);
             toast.show('Failed to create content', this.toasTimeout, 'red');
         });
 
-        this.repo.OnContentModified.subscribe(mod => {
+        this.repo.Events.OnContentModified.subscribe(mod => {
             console.log("OnContentModified", mod);
             toast.show('Content modified', this.toasTimeout, 'green');
         });
-        this.repo.OnContentModificationFailed.subscribe(mod => {
+        this.repo.Events.OnContentModificationFailed.subscribe(mod => {
             console.log("OnContentModificationFailed", mod);
             toast.show('Content modified', this.toasTimeout, 'red');
         });
 
 
-        this.repo.OnContentDeleted.subscribe(del => {
+        this.repo.Events.OnContentDeleted.subscribe(del => {
             console.log("OnContentDeleted", del);
             toast.show('Content deleted', this.toasTimeout, 'red');
         });
